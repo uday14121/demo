13bc02354f20f3da94616d9b7af4ac5a5300a1a3
uday sonvane <udaysonawane1412@gmail.com>
2025-08-06 00:29:00 +0530

    add new files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # demo
 the demo of github.
+
+# teacher 
+the teacher name is shradha khapra
---
+++ app.js
@@ -0,0 +1,22 @@
+ let input = document.querySelector("input")
+ let allbtn = document.querySelectorAll("button")
+ 
+
+
+// function oparation(user,a,b) {
+//     let = arr = ["+" , "-" , "*" , "/"];
+   
+//     if (user == "+") {
+//         return a + b ;
+//     }else if (user == "-") {
+//         return a - b ;
+//     } else if (user == "*") {
+//         return a *b ;
+//     } else {
+//         return a / b
+//     }
+
+// }
+// let res = oparation("+" , 5 ,7)
+// console.log(res)
+
